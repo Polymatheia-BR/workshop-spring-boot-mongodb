19ecb2df4c779a28347b2fb343c1aa7817f45064
Victor Ferreira França <vffvmode@gmail.com>
2022-04-13 03:37:27 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/victor/workshopmongo/domain/Post.java
+++ src/main/java/com/victor/workshopmongo/domain/Post.java
@@ -1,7 +1,9 @@
 package com.victor.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
@@ -19,6 +21,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 	}
@@ -70,6 +74,14 @@ public class Post implements Serializable {
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
